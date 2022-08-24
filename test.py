80712af0af5fc157090cb45e8425d8e8e5c39e7a
Александр Посаженников <101997038+AposazhennikovDS@users.noreply.github.com>
2022-08-25 01:55:00 +0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import aiogram_calendar
-import sys
-import os
-for path in sys.path:
-   if os.path.exists(os.path.join(path, 'aiogram_calendar')):
-      print('some_module is here: {}'.format(path))
